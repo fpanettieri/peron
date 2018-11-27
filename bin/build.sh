e2cc714502306aebc7ee7f0171b24be32af1969b
Fabio Panettieri <fpanettieri@gmail.com>
2018-11-26 23:21:40 -0300

    tls build wip

--- bin/build.sh
+++ bin/build.sh
@@ -2,15 +2,16 @@
 mkdir -p build
 pushd build > /dev/null
 
-export CC_WARNINGS="-Wall -Wextra -Wl,-map,mem.map -pedantic -Werror -Wshadow -Wstrict-overflow -fno-strict-aliasing"
-export CC_OPTIM="-O2 -Os -march=native"
-export CC_DEFINES="-DINTERNAL"
+CC_WARNINGS="-Wall -Wextra -Wl,-map,mem.map -pedantic -Werror -Wshadow -Wstrict-overflow -fno-strict-aliasing"
+CC_OPTIM="-O2 -Os -march=native"
+CC_DEFINES="-DINTERNAL"
 
 FLAGS="$CC_WARNINGS $CC_OPTIM $CC_DEFINES"
+
 INC_PATH="-I../inc"
 SRC_PATH="../src"
 LIB_PATH=""
-LIBS=""
+LIBS="-ltls -lssl -lcrypto"
 
 rm -rf *
 
