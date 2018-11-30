18ff23d1443a137d481ccffea93bf06297070117
Fabio Panettieri <fpanettieri@gmail.com>
2018-11-30 17:01:58 -0300

    comm skeleton

--- inc/net.h
+++ inc/net.h
@@ -10,4 +10,7 @@ typedef struct {
 } Net;
 
 void net_init(Net* net);
+void net_connect(Net* net, const char* host, const char* port);
+void net_read(Net* net, void* buf, SZT buflen, SZT* read);
+void net_write(Net* net, const void* buf, SZT buflen, SZT* written);
 void net_destroy(Net* net);
--- src/net.c
+++ src/net.c
@@ -2,27 +2,47 @@
 
 void net_init(Net* net)
 {
-  if (tls_init() != 0) { goto tls_err; }
+  U8 error = 0;
+  error = tls_init();
+  assert(error == 0);
 
   net->client = tls_client();
-  if (net->client == NULL) { goto tls_err; }
+  assert(net->client);
 
   net->cfg = tls_config_new();
-  if (net->cfg == NULL) { goto tls_err; }
+  assert(net->cfg);
 
-  if (tls_config_set_ciphers(net->cfg, "secure") != 0) { goto tls_err; }
+  error = tls_config_set_ciphers(net->cfg, "secure");
+  assert(error == 0);
 
   // TODO: set CA file
   // if (tls_config_set_ca_file(*net->cfg, "peron.pem") != 0) { goto tls_err; }
   tls_config_insecure_noverifycert(net->cfg);
   tls_config_insecure_noverifyname(net->cfg);
 
-  if (tls_configure(net->client, net->cfg) != 0) { goto tls_err; }
+  error = tls_configure(net->client, net->cfg);
+  assert(error);
 
   net->initialized = true;
+}
 
-tls_err:
-  fprintf(stderr, "%s\n", tls_error(net->client));
+void net_connect(Net* net, const char* host, const char* port)
+{
+  assert(net && net->initialized);
+  U8 error = tls_connect(net->client, host, port);
+  assert(error == 0);
+}
+
+void net_read(Net* net, void* buf, SZT buflen, SZT* read)
+{
+  assert(net && net->initialized);
+  assert(buf && buflen && read);
+}
+
+void net_write(Net* net, const void* buf, SZT buflen, SZT* written)
+{
+  assert(net && net->initialized);
+  assert(buf && buflen && written);
 }
 
 void net_destroy(Net* net)
@@ -30,15 +50,11 @@ void net_destroy(Net* net)
   assert(net && net->initialized);
   assert(net->client && net->cfg);
 
-  if (tls_close(net->client) != 0) { goto tls_err; }
-	tls_free(net->client);
-	tls_config_free(net->cfg);
+  U8 error = tls_close(net->client);
+  assert(error == 0);
 
-tls_err:
-  fprintf(stderr, "%s\n", tls_error(net->client));
+  tls_free(net->client);
+  tls_config_free(net->cfg);
+
+  net->initialized = false;
 }
-
-
-// write
-// read
-// assert(net && net->initialized);
