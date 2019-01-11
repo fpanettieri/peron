e3e1c6e77638b658286f6c77542c4045cd018917
Fabio Panettieri <fpanettieri@gmail.com>
2019-01-11 12:49:24 -0300

    ml proto

---
+++ ml/logger.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const colors = {
+  base: {
+    reset: "\x1b[0m",
+    bright: "\x1b[1m",
+    dim: "\x1b[2m",
+    underscore: "\x1b[4m",
+    blink: "\x1b[5m",
+    reverse: "\x1b[7m",
+    hidden: "\x1b[8m"
+  },
+
+  fg: {
+    black: "\x1b[30m",
+    red: "\x1b[31m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    blue: "\x1b[34m",
+    magenta: "\x1b[35m",
+    cyan: "\x1b[36m",
+    white: "\x1b[37m"
+  },
+
+  bg: {
+    black: "\x1b[40m",
+    red: "\x1b[41m",
+    green: "\x1b[42m",
+    yellow: "\x1b[43m",
+    blue: "\x1b[44m",
+    magenta: "\x1b[45m",
+    cyan: "\x1b[46m",
+    white: "\x1b[47m"
+  }
+}
+
+class Logger
+{
+  constructor(prefix) { this.prefix = prefix; }
+  info ()  { console.info(colors.fg.green + this.prefix, ...arguments, colors.base.reset); }
+  log ()   { console.log(this.prefix, ...arguments); }
+  warn ()  { console.warn(colors.fg.yellow + this.prefix, ...arguments, colors.base.reset); }
+  error () { console.error(colors.fg.red + this.prefix, ...arguments, colors.base.reset); }
+}
+
+module.exports = Logger;
---
+++ ml/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const ws = require('ws');
+// const core = require('./core');
+const logger = require('./logger');
+
+const log = new logger(`[BitMEX/Peron-ml]`);
