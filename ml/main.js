2d1351d083721ebe51c9d596a050d3226c327f74
Fabio Panettieri <fpanettieri@gmail.com>
2019-01-11 19:51:50 -0300

    testing base req

--- ml/main.js
+++ ml/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const crypto = require('crypto');
-const http = require('http');
+const https = require('https');
 const logger = require('./logger');
 
 const log = new logger(`[BitMEX/Peron-ml]`);
@@ -14,13 +14,16 @@ const log = new logger(`[BitMEX/Peron-ml]`);
 //   args: [ process.env.BITMEX_KEY, expires, signature ]
 // }
 
+const method = 'GET'
+log.log('method', method);
+
 const expires = ~~(Date.now() / 1000 + 24 * 60 * 60);
 log.log('expires', expires);
 
-const url = '/api/v1/quote?count=100&reverse=false';
-log.log('url', url);
+const path = '/api/v1/quote?count=100&reverse=false';
+log.log('path', path);
 
-const signature = crypto.createHmac('sha256', process.env.BITMEX_SECRET).update(`GET${url}${expires}`).digest('hex');
+const signature = crypto.createHmac('sha256', process.env.BITMEX_SECRET).update(`${method}${path}${expires}`).digest('hex');
 log.log('signature', signature);
 
 const headers = {
@@ -34,17 +37,16 @@ const headers = {
 log.log(headers);
 
 const options = {
-  protocol: 'https:'
   host: 'testnet.bitmex.com',
-  path: url,
-  headers: headers,
-  method: 'GET',
-  body: ''
+  path: path,
+  method: method,
+  headers: headers
 };
 
-const req = http.get(options);
-req.end();
+const req = https.get(options);
 req.once('response', (res) => {
-
+  log.log('response');
   log.log(res);
 });
+
+req.end();
