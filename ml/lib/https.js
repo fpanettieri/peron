46df00cb9818014bbb097941210c33769c81c5ee
Fabio Panettieri <fpanettieri@gmail.com>
2019-01-13 23:13:49 -0300

    https library

--- ml/lib/https.js
+++ ml/lib/https.js
@@ -1,23 +1,47 @@
 'use strict';
 
 const https = require('https');
-const url = require('url');
 const logger = require('./logger');
 
 const log = new logger(`[lib/https]`);
 
-async function get (_url, opts)
+async function send (url, data, opts)
 {
-  const u = url.parse(_url);
-  log.log('parsed url', u);
-  const req = https.request(options);
-  return {a: 'a'};
+  return new Promise(function(resolve, reject) {
+    const req = https.request(url, opts);
+    log.log('opts', opts);
+
+    req.on('error', reject);
+    req.once('response', (res) => {
+      res.setEncoding('utf8');
+
+      const out = {
+        status: {code: res.statusCode, msg: res.statusMessage},
+        headers: res.headers,
+        body: ''
+      };
+
+      res.on('data', (chunk) => { out.body += chunk; });
+      res.on('end', () => { resolve(out); });
+    });
+
+    if (data) { req.write(data); }
+    req.end();
+  });
 }
 
+async function get (url, opts)
+{
+  opts = opts || {};
+  opts.method = 'GET';
+  return send(url, null, opts);
+}
 
 async function post (url, data, opts)
 {
-
+  opts = opts || {};
+  opts.method = 'POST';
+  return send(url, data, opts);
 }
 
 module.exports = {
