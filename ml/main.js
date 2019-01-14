fd271f1518b4938a4e56c89f1645f16655ed7ecc
Fabio Panettieri <fpanettieri@gmail.com>
2019-01-13 21:27:22 -0300

    back to bitmex

--- ml/main.js
+++ ml/main.js
@@ -1,9 +1,61 @@
 'use strict';
 
-
+const crypto = require('crypto');
+const https = require('https');
 const logger = require('./logger');
 
-const log = new logger(`[Deribit/Peron-ml]`);
+const log = new logger(`[BitMEX/Peron-ml]`);
 
-log.log('api secret', process.env.DERIBIT_SECRET);
-log.log('api key', process.env.DERIBIT_KEY);
+// const expires = ~~(Date.now() / 1000 + 24 * 60 * 60);
+// log.log('GET/realtime' + expires);
+// const signature = crypto.createHmac('sha256', process.env.BITMEX_SECRET).update('GET/realtime' + expires).digest('hex');
+// const auth_params = {
+//   op: 'authKeyExpires',
+//   args: [ process.env.BITMEX_KEY, expires, signature ]
+// }
+
+const method = 'GET'
+log.log('method', method);
+
+const expires = ~~(Date.now() / 1000 + 24 * 60 * 60);
+log.log('expires', expires);
+
+const path = '/api/v1/quote';
+// const path = '/';
+log.log('path', path);
+
+const signature = crypto.createHmac('sha256', process.env.BITMEX_SECRET).update(`${method}${path}${expires}`).digest('hex');
+log.log('signature', signature);
+
+const headers = {
+  'content-type' : 'application/json',
+  'Accept': 'application/json',
+  'X-Requested-With': 'XMLHttpRequest',
+  'api-expires': expires,
+  'api-key': process.env.BITMEX_KEY,
+  'api-signature': signature
+};
+log.log(headers);
+
+const options = {
+  host: 'testnet.bitmex.com',
+  path: path,
+  method: method,
+  headers: headers
+};
+
+const req = https.request(options);
+req.on('error', (err) => { log.error(`problem with request: ${e.message}`); });
+
+req.once('response', (res) => {
+  log.log(res.statusCode, res.statusMessage, res.headers);
+  // log.log(res);
+
+  // let data = '';
+  // res.setEncoding('utf8');
+  // res.on('data', (chunk) => { data += chunk; });
+  // res.on('end', () => { log.info('end', data); });
+});
+
+// req.write(postData);
+req.end();
