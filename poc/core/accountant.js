7a2e0facc0c8a14de12b71b8c90aab968167f444
Fabio Panettieri <fpanettieri@gmail.com>
2019-02-12 18:57:20 -0300

    debugging update

--- poc/core/accountant.js
+++ poc/core/accountant.js
@@ -16,7 +16,7 @@ function plug (_bb, _db)
   log.log('plugging');
   bb = _bb;
   db = _db;
-  bb.on('MarginUpdated', onBalanceUpdated);
+  bb.on('MarginUpdated', onMarginUpdated);
   bb.on('PositionSynced', onPositionSynced);
   bb.on('PositionOpened', onPositionOpened);
   bb.on('PositionUpdated', onPositionUpdated);
@@ -43,16 +43,21 @@ function onPositionOpened (data)
 
 function onPositionUpdated (data)
 {
+  console.log('=======================');
+
   log.info(`position updated`);
   for (let i = 0; i < data.length; i++) {
     let pos = data[i];
-    let stored = positions.find(p => p.symbol == pos.symbol && p.account == pos.account && p.currency == pos.currency);
-    let merged = {...stored, ...pos};
+    console.log('\n/// POS\n', pos, '\n');
 
-    console.log('\n\n',pos,'\n\n');
-    console.log('\n\n',stored,'\n\n');
-    console.log('\n\n',merged,'\n\n');
+    let idx = positions.findIndex(p => p.symbol == pos.symbol && p.account == pos.account && p.currency == pos.currency);
+    console.log('\n/// STORED\n', positions[idx], '\n');
+
+    positions[idx] = {...positions[idx], ...pos};
+    console.log('\n/// MERGED\n', positions[idx], '\n');
   }
+
+  console.log('=======================');
 }
 
 function onPositionClosed (data)
