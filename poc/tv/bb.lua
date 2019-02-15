06437bf279430ad5ebb5c2b4d41cf58410e7d71c
Fabio Panettieri <fpanettieri@gmail.com>
2019-02-14 23:43:10 -0300

    multi bb

--- poc/tv/bb.lua
+++ poc/tv/bb.lua
@@ -1,16 +1,25 @@
 //@version=3
 study(shorttitle="BB", title="Bollinger Bands", overlay=true)
-length = input(30, minval=1)
-src = input(close, title="Source")
-mult = input(2.0, minval=0.001, maxval=50)
+src = close
+short_length = 20
+long_length = 200
+mult = 2
 
-basis = sma(src, length)
-dev = mult * stdev(src, length)
+short_basis = sma(src, short_length)
+long_basis = sma(src, long_length)
 
-upper = basis + dev
-lower = basis - dev
+short_dev = mult * stdev(src, short_length)
+long_dev = mult * stdev(src, long_length)
 
-plot(basis, color=red)
+short_upper = short_basis + short_dev
+short_lower = short_basis - short_dev
+
+long_upper = long_basis + long_dev
+long_lower = long_basis - long_dev
+
+plot(short_basis, color=red)
+plot(long_basis, color=red)
+
 // p1 = plot(upper, color=blue)
 // p2 = plot(lower, color=blue)
 // fill(p1, p2)
