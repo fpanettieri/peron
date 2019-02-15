43ebacb6aa1c5820325e51485f087c7ff778a0a9
Fabio Panettieri <fpanettieri@gmail.com>
2019-02-14 23:27:54 -0300

    bollinger bands

--- poc/tv/bb_mm.lua
+++ poc/tv/bb_mm.lua
@@ -1,6 +1,10 @@
-//@version=2
-strategy("test")
-if n>4000
-    strategy.entry("buy", strategy.long, 10, when=strategy.position_size <= 0)
-    strategy.entry("sell", strategy.short, 10, when=strategy.position_size > 0)
-plot(strategy.equity)
+//@version=3
+strategy("Bollinger Bands Market Maker", overlay=true)
+
+longCondition = crossover(sma(close, 14), sma(close, 28))
+if (longCondition)
+  strategy.entry("My Long Entry Id", strategy.long)
+
+shortCondition = crossunder(sma(close, 14), sma(close, 28))
+if (shortCondition)
+  strategy.entry("My Short Entry Id", strategy.short)
