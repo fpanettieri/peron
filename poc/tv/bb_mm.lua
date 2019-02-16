5a398660fd5499ec9943ab457e2067a965c5f7de
Fabio Panettieri <fpanettieri@gmail.com>
2019-02-16 10:28:26 -0300

    Market maker algo v1

--- poc/tv/bb_mm.lua
+++ poc/tv/bb_mm.lua
@@ -1,10 +1,26 @@
 //@version=3
 strategy("Bollinger Bands Market Maker", overlay=true)
+source = close
+length = input(20, minval=1)
+mult = input(2.0, minval=0.001, maxval=50)
+direction = input(0, title = "Strategy Direction", type=integer, minval=-1, maxval=1)
 
-longCondition = crossover(sma(close, 14), sma(close, 28))
-if (longCondition)
-  strategy.entry("My Long Entry Id", strategy.long)
+strategy.risk.allow_entry_in(direction == 0 ? strategy.direction.all : (direction < 0 ? strategy.direction.short : strategy.direction.long))
 
-shortCondition = crossunder(sma(close, 14), sma(close, 28))
-if (shortCondition)
-  strategy.entry("My Short Entry Id", strategy.short)
+basis = sma(source, length)
+dev = mult * stdev(source, length)
+
+upper = basis + dev
+lower = basis - dev
+
+if (crossover(source, lower))
+    strategy.entry("BBandLE", strategy.long, stop=lower, oca_name="BollingerBands", oca_type=strategy.oca.cancel, comment="BBandLE")
+else
+    strategy.cancel(id="BBandLE")
+
+if (crossunder(source, upper))
+    strategy.entry("BBandSE", strategy.short, stop=upper, oca_name="BollingerBands", oca_type=strategy.oca.cancel, comment="BBandSE")
+else
+    strategy.cancel(id="BBandSE")
+
+//plot(strategy.equity, title="equity", color=red, linewidth=2, style=areabr)
