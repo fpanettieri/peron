c8ce0de02e9eff6df29b4ad59238ab1918c71c46
Fabio Panettieri <fpanettieri@gmail.com>
2019-02-16 11:11:48 -0300

    auto stop?

--- poc/tv/bb_mm.lua
+++ poc/tv/bb_mm.lua
@@ -1,11 +1,8 @@
 //@version=3
-strategy("Bollinger Bands Market Maker", overlay=true)
+strategy("Bollinger Bands Strategy", overlay=true)
 source = close
 length = input(20, minval=1)
 mult = input(2.0, minval=0.001, maxval=50)
-direction = input(0, title = "Strategy Direction", type=integer, minval=-1, maxval=1)
-
-strategy.risk.allow_entry_in(direction == 0 ? strategy.direction.all : (direction < 0 ? strategy.direction.short : strategy.direction.long))
 
 basis = sma(source, length)
 dev = mult * stdev(source, length)
@@ -13,13 +10,16 @@ dev = mult * stdev(source, length)
 upper = basis + dev
 lower = basis - dev
 
+buyEntry = crossover(source, lower)
+sellEntry = crossunder(source, upper)
+
 if (crossover(source, lower))
-    strategy.entry("BBandLE", strategy.long, stop=lower, oca_name="BollingerBands", oca_type=strategy.oca.cancel, comment="BBandLE")
+    strategy.entry("BBandLE", strategy.long, stop=basis, oca_name="BollingerBands", oca_type=strategy.oca.cancel, comment="BBandLE")
 else
     strategy.cancel(id="BBandLE")
 
 if (crossunder(source, upper))
-    strategy.entry("BBandSE", strategy.short, stop=upper, oca_name="BollingerBands", oca_type=strategy.oca.cancel, comment="BBandSE")
+    strategy.entry("BBandSE", strategy.short, stop=basis, oca_name="BollingerBands", oca_type=strategy.oca.cancel, comment="BBandSE")
 else
     strategy.cancel(id="BBandSE")
 
