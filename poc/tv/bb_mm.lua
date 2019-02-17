9b12085faee68b9600da6218f85e2d7de49c06dc
Fabio Panettieri <fpanettieri@gmail.com>
2019-02-17 13:45:06 -0300

    strategy code cleanup

--- poc/tv/bb_mm.lua
+++ poc/tv/bb_mm.lua
@@ -1,24 +1,29 @@
 //@version=3
+
 strategy("BB MM", overlay=true)
-source = close
+
+// Revision:        1
+// Author:          @getplatita
+
+// === INPUT SMA ===
 length = input(50, minval=1)
 mult = input(2.1, minval=0.001, maxval=50)
 
-basis = ema(source, length)
-dev = mult * stdev(source, length)
+basis = ema(close, length)
+dev = mult * stdev(close, length)
 
 upper = basis + dev
 lower = basis - dev
 
-buyEntry = crossover(source, lower)
-sellEntry = crossunder(source, upper)
+buyEntry = crossover(close, lower)
+sellEntry = crossunder(close, upper)
 
-if (crossover(source, lower))
+if (crossover(close, lower))
     strategy.entry("BBandLE", strategy.long, stop=lower, oca_name="BollingerBands", oca_type=strategy.oca.cancel, comment="BBandLE")
 else
     strategy.cancel(id="BBandLE")
 
-if (crossunder(source, upper))
+if (crossunder(close, upper))
     strategy.entry("BBandSE", strategy.short, stop=upper, oca_name="BollingerBands", oca_type=strategy.oca.cancel, comment="BBandSE")
 else
     strategy.cancel(id="BBandSE")
