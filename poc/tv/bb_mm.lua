bbe283e9213b188121b215f161f569533319fc23
Fabio Panettieri <fpanettieri@gmail.com>
2019-02-19 12:20:48 -0300

    improved strategy

--- poc/tv/bb_mm.lua
+++ poc/tv/bb_mm.lua
@@ -7,7 +7,7 @@ period = input(20, minval=1)
 mult = input(2, minval=0.001, maxval=50)
 
 // === PARAMS ===
-basis = ma(close, period)
+basis = sma(close, period)
 dev = mult * stdev(close, period)
 upper = basis + dev
 lower = basis - dev
@@ -20,7 +20,7 @@ strategy.entry("S", strategy.short, when = crossunder(close, upper) and close > 
 strategy.close("S", when = low < basis)
 
 // === PLOT ===
-plot(basis, color=red, title='ma')
-p1 = plot(upper, color=blue, title='upper')
-p2 = plot(lower, color=blue, title='lower')
+plot(basis, color=white, title='ma')
+p1 = plot(upper, color=white, title='upper')
+p2 = plot(lower, color=white, title='lower')
 fill(p1, p2)
