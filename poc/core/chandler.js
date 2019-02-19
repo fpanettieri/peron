226b6fc0480633ea111f34b12adb074d8c25c228
Fabio Panettieri <fpanettieri@gmail.com>
2019-02-19 17:57:01 -0300

    base chandler algo

--- poc/core/chandler.js
+++ poc/core/chandler.js
@@ -5,8 +5,11 @@ const utils = require('../lib/utils');
 const logger = require('../lib/logger');
 const log = new logger('[core/chandler]');
 
+const CLOSE_OFFSET = cfg.chandler.offset * 1000;
+const CANDLE_STEP = utils.intervalToMs(cfg.timeframe);
+
 let bb = null;
-let partial = null;
+let candle = null;
 
 function plug (_bb)
 {
@@ -14,16 +17,27 @@ function plug (_bb)
   bb = _bb;
   bb.on('CandleReceived', onCandleReceived);
   bb.on('TradeReceived', onTradeReceived);
+
+  let timeout = CANDLE_STEP - (Date.now() % CANDLE_STEP) + CLOSE_OFFSET;
+  setTimeout(closeCandle, timeout);
 }
 
 function onCandleReceived (c)
 {
-  log.log(c);
+  if (candle) { return; }
+  candle = c;
 }
 
 function onTradeReceived (t)
 {
-  log.log('trade');
+  if (!candle) { return; }
+  candle.c = t.price;
+}
+
+function closeCandle ()
+{
+  log.log('CLOSING CANDLE!');
+  setTimeout(closeCandle, CANDLE_STEP);
 }
 
 module.exports = { plug: plug }
