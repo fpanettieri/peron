1ae9070bd5b29922a8e07d0c92d6c335fd4cfc83
Fabio Panettieri <fpanettieri@gmail.com>
2019-02-24 20:36:21 -0300

    more debugging

--- poc/doc/broker_params.js
+++ poc/doc/broker_params.js
@@ -4,10 +4,12 @@
   orderQty: 1,
   price: 3000, // Magic, find best possible price
   timeInForce: 'GoodTillCancel',
-  clOrdID: 1, // Track orders on the broker
+  clOrdID: `ag-asdasd13`, // Track orders on the broker
   ordType: 'Limit',
   execInst: 'ParticipateDoNotInitiate'
 }
 
 
 ParticipateDoNotInitiate: If this order would have executed on placement, it will cancel instead.
+
+curl -X POST --header 'Content-Type: application/x-www-form-urlencoded' --header 'Accept: application/json' --header 'X-Requested-With: XMLHttpRequest' -d 'symbol=XBTUSD&side=Buy&orderQty=1&price=3000&clOrdID=ag-1231231c&ordType=Limit&timeInForce=GoodTillCancel&execInst=ParticipateDoNotInitiate' 'https://testnet.bitmex.com/api/v1/order'
