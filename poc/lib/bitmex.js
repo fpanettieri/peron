275d9ef639f10871a3ac4ee8c8ba2743b43d3b90
Fabio Panettieri <fpanettieri@gmail.com>
2019-02-24 23:08:44 -0300

    json data

--- poc/lib/bitmex.js
+++ poc/lib/bitmex.js
@@ -15,7 +15,6 @@ async function api (opts, params)
   // ~~(n) == fast toInt
   const expires = ~~(Date.now() / 1000 + AUTH_EXPIRES);
   const path = `/api/v1/${opts.api}`;
-  // const data = Object.entries(params).map(([k, v]) => `${k}=${v}`).join('&');
   const data = JSON.stringify(params);
   const unsigned = `${opts.method}${path}${expires}${data}`;
   const signature = crypto.createHmac('sha256', process.env.BITMEX_SECRET).update(unsigned).digest('hex');
