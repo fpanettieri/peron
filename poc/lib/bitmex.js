b8b3255232f6352f0c3da9fd0fb5be2105ae34a8
Fabio Panettieri <fpanettieri@gmail.com>
2019-02-25 00:00:01 -0300

    bitmex api get/post

--- poc/lib/bitmex.js
+++ poc/lib/bitmex.js
@@ -12,36 +12,31 @@ const log = new Logger('[lib/bitmex]');
 
 async function api (opts, params)
 {
-  log.log('=================================================================');
-  log.log('api', opts, params);
-
   // ~~(n) == fast toInt
   const expires = ~~(Date.now() / 1000 + AUTH_EXPIRES);
-  const path = `/api/v1/${opts.api}`;
-  // const data = JSON.stringify(params);
   const data = Object.entries(params).map(([k, v]) => `${k}=${v}`).join('&');
-  const unsigned = `${opts.method}${path}${expires}${data}`;
-  const signature = crypto.createHmac('sha256', process.env.BITMEX_SECRET).update(unsigned).digest('hex');
 
-  log.log('expires', expires);
-  log.log('path', path);
-  log.log('data', data);
-  log.log('unsigned', unsigned);
-  log.log('signature', signature);
+  let url = `/api/v1/${opts.api}`;
+  let body = '';
+
+  if (opts.method == 'GET') {
+    url += `?${data}`;
+  } else {
+    body = data;
+  }
+
+  const unsigned = `${opts.method}${url}${expires}${body}`;
+  const signature = crypto.createHmac('sha256', process.env.BITMEX_SECRET).update(unsigned).digest('hex');
 
   const headers = {
     'api-expires': expires,
     'api-key': process.env.BITMEX_KEY,
     'api-signature': signature,
-    'content-type': 'application/x-www-form-urlencoded',
-    // 'content-type' : 'application/json',
-    // 'Accept': 'application/json',
-    // 'X-Requested-With': 'XMLHttpRequest'
+    'content-type': 'application/x-www-form-urlencoded'
   };
 
   const host = `https://${opts.testnet ? 'testnet' : 'www'}.bitmex.com`;
-  const rsp = await https.send(`${host}${path}`, data, {method: opts.method, headers: headers});
-  log.log(rsp);
+  const rsp = await https.send(`${host}${url}`, body, {method: opts.method, headers: headers});
   rsp.body = JSON.parse(rsp.body);
 
   log.warn('x-ratelimit-remaining', rsp.headers['x-ratelimit-remaining']);
