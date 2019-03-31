e90e12f1b004f03a1e26bf5a33be6ae0fb7d42ce
Fabio Panettieri <fpanettieri@gmail.com>
2019-03-30 22:39:43 -0300

    testing auditor code

--- poc/core/auditor.js
+++ poc/core/auditor.js
@@ -4,26 +4,19 @@ const logger = require('../lib/logger');
 const log = new logger('[core/auditor]');
 
 let bb = null;
-let start = 0;
 
 function plug (_bb)
 {
   bb = _bb;
-  bb.on('CandleClosed', onCandleClosed);
-  bb.on('BuyContract', onPreMarket);
-  bb.on('SellContract', onPreMarket);
-  // TODO: On TradeExecuted log the time elapsed
+
+  bb.on('CandleClosed', noop);
+  bb.on('BuyContract', noop);
+  bb.on('SellContract', noop);
 }
 
-function onCandleClosed (c)
+function noop ()
 {
-  log.log('onCandleClosed');
-  start = Date.now();
-}
 
-function onPreMarket (t)
-{
-  log.log(`pre-market: ${Date.now() - start}ms`);
 }
 
 module.exports = { plug: plug };
