02cdfaacc5bd6f45fbc0abf0d8a3f971e1ca0c95
Fabio Panettieri <fpanettieri@gmail.com>
2019-04-08 00:24:22 -0300

    cleaner logs

--- poc/lib/logger.js
+++ poc/lib/logger.js
@@ -1,39 +1,5 @@
 'use strict';
 
-const colors = {
-  base: {
-    reset: "\x1b[0m",
-    bright: "\x1b[1m",
-    dim: "\x1b[2m",
-    underscore: "\x1b[4m",
-    blink: "\x1b[5m",
-    reverse: "\x1b[7m",
-    hidden: "\x1b[8m"
-  },
-
-  fg: {
-    black: "\x1b[30m",
-    red: "\x1b[31m",
-    green: "\x1b[32m",
-    yellow: "\x1b[33m",
-    blue: "\x1b[34m",
-    magenta: "\x1b[35m",
-    cyan: "\x1b[36m",
-    white: "\x1b[37m"
-  },
-
-  bg: {
-    black: "\x1b[40m",
-    red: "\x1b[41m",
-    green: "\x1b[42m",
-    yellow: "\x1b[43m",
-    blue: "\x1b[44m",
-    magenta: "\x1b[45m",
-    cyan: "\x1b[46m",
-    white: "\x1b[47m"
-  }
-};
-
 const LB_INTERVAL = 10 * 1000;
 
 class Logger
@@ -43,21 +9,19 @@ class Logger
     this.t = Date.now();
   }
 
-  lb () {
+  format (fn, level, args) {
     if (Date.now() - this.t > LB_INTERVAL) { console.log('\n'); }
     this.t = Date.now();
+    const date = new Date().toISOString();
+    fn(`[${date} ${level} ${this.prefix}]`, ...args);
   }
 
-  date ()  {
-    return `[${new Date().toISOString()}]`;
-  }
-
-  debug () { this.lb(); console.info(colors.fg.cyan + this.date(), this.prefix, ...arguments, colors.base.reset); }
-  log ()   { this.lb(); console.log(this.date(), this.prefix, ...arguments); }
-  info ()  { this.lb(); console.info(colors.fg.green + this.date(), this.prefix, ...arguments, colors.base.reset); }
-  warn ()  { this.lb(); console.warn(colors.fg.yellow + this.date(), this.prefix, ...arguments, colors.base.reset); }
-  error () { this.lb(); console.error(colors.fg.red + this.date(), this.prefix, ...arguments, colors.base.reset); }
-  fatal () { this.lb(); console.error(colors.fg.red + this.date(), this.prefix, ...arguments, colors.base.reset); process.exit(-1); }
+  debug () { this.format(console.log, 'DEBUG', arguments)}
+  log ()   { this.format(console.log, 'LOG', arguments)}
+  info ()  { this.format(console.info, 'INFO', arguments)}
+  warn ()  { this.format(console.warn, 'WARN', arguments)}
+  error () { this.format(console.error, 'ERROR', arguments)}
+  fatal () { this.format(console.error, 'FATAL', arguments); process.exit(-1); }
 }
 
 module.exports = Logger;
