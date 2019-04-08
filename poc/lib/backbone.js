096a169940837f835ea5ef7adc15b7024e0f9e09
Fabio Panettieri <fpanettieri@gmail.com>
2019-04-08 03:14:12 -0300

    detailed log

--- poc/lib/backbone.js
+++ poc/lib/backbone.js
@@ -2,20 +2,10 @@
 
 const EventEmitter = require('events');
 
-const logger = require('./logger');
-const log = new logger('lib/backbone');
-
 class Backbone extends EventEmitter
 {
-  constructor ()
-  {
-    log.log('initializing');
-    super();
-  }
-
   chain (a, b)
   {
-    log.log(`chaining ${a} => ${b}`);
     this.on(a, () => this.emit(b));
   }
 
--- poc/peron.js
+++ poc/peron.js
@@ -8,16 +8,23 @@ const backbone = require('./lib/backbone');
 const modules = [];
 
 (async () => {
+  const log = new logger(`[Peron/main]`);
+  log.info('peronizando');
+
   const bb = new backbone();
 
   for (let i = 0; i < cfg.modules.length; i++) {
-    const m = require(`./${cfg.modules[i]}`);
+    const name = cfg.modules[i];
+    const m = require(`./${name}`);
+    log.info(`loading module ${name}`);
+
     m.plug(bb);
     modules.push(m);
   }
 
   for (let i = 0; i < cfg.backbone.chain.length; i++) {
     const io = cfg.backbone.chain[i];
+    log.log(`chaining ${io[0]} => ${io[1]}`);
     bb.chain(io[0], io[1]);
   }
 
