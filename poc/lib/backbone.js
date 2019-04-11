3f802106aa8d4231361e47285b5478fb85887857
Fabio Panettieri <fpanettieri@gmail.com>
2019-04-11 16:22:26 -0300

    more logs

--- poc/lib/backbone.js
+++ poc/lib/backbone.js
@@ -19,15 +19,16 @@ class Backbone extends EventEmitter
   emit ()
   {
     if (arguments[0] == 'TradeContract') { console.log('##########################################################'); }
+    console.log('[> queueing]', arguments[0]);
     this.queue.push(arguments);
 
-    if (this.queue.length > 1) { console.log('shortcut exit, already running'); return; }
+    if (this.queue.length > 1) { console.log('shortcut exit, already running', `queue size: (${this.queue.length})`); return; }
 
     while (this.queue.length > 0) {
       let args = this.queue[0];
-      console.log('start ', args[0], `queue size: (${this.queue.length})`);  // remove this
+      console.log(`[+ start] ${args[0]} queue size: (${this.queue.length})`);  // remove this
       super.emit.apply(this, args);
-      console.log('end ', args[0], `queue size: (${this.queue.length})`, '\n');  // remove this
+      console.log(`[- end] ${args[0]} queue size: (${this.queue.length})`, '\n');  // remove this
       this.queue.shift();
     }
   }
