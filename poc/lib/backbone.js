fa9c870583e340c42c94deddccbbadae57c89faf
Fabio Panettieri <fpanettieri@gmail.com>
2019-04-11 17:19:37 -0300

    serialized events

--- poc/lib/backbone.js
+++ poc/lib/backbone.js
@@ -16,23 +16,16 @@ class Backbone extends EventEmitter
     this.on(a, () => this.emit(b));
   }
 
+  // Serialize event resolution
   emit ()
   {
-    if (arguments[0] == 'TradeContract') { console.log('##########################################################'); }
-    console.log('[> QUEUEING]   ', arguments[0]);
     this.queue.push(arguments);
+    if (this.queue.length > 1) { return; }
 
-    if (this.queue.length > 1) { console.log('[X SHORTCUT]    already running', `queue size: (${this.queue.length})`); return; }
-
-    console.log('\n[. ITERATING]');
     while (this.queue.length > 0) {
-      let args = this.queue[0];
-      console.log(`[+ START]       ${args[0]} queue size: (${this.queue.length})`);    // remove this
-      super.emit.apply(this, args);
+      super.emit.apply(this, this.queue[0]);
       this.queue.shift();
-      console.log(`[- END]         ${args[0]} queue size: (${this.queue.length})`);  // remove this
     }
-    console.log('[. DONE]\n');
   }
 }
 
