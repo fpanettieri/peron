a5bfc04646082119ede2801f5f4f557ad1d9f698
Fabio Panettieri <fpanettieri@gmail.com>
2019-04-13 16:43:13 -0300

    adding ntz

--- poc/tv/bb_ntz_mm.lua
+++ poc/tv/bb_ntz_mm.lua
@@ -3,13 +3,16 @@
 strategy("BB NTZ MM", overlay=true)
 
 // === INPUT ===
-period = input(20, minval=1)
-year  = input(defval = 2019, title = "From Year", minval = 2017)
-month = input(defval = 4, title = "From Month", minval = 1, maxval = 12)
-day   = input(defval = 12, title = "From Day", minval = 1, maxval = 31)
+period  = input(20, minval=1)
+year    = input(defval = 2019, title = "From Year", minval = 2017)
+month   = input(defval = 4, title = "From Month", minval = 1, maxval = 12)
+day     = input(defval = 12, title = "From Day", minval = 1, maxval = 31)
+src     = input(close, title="Source")
+bb_mul  = input(2.0, minval=0.001, maxval=50)
+ntz_mul = input(0.7, minval=0.001, maxval=50)
 
 // === PARAMS ===
-basis = sma(close, period)
+basis = sma(src, period)
 start = timestamp(year, month, day, 00, 00)
 
 // === EXECUTION ===
@@ -20,4 +23,12 @@ strategy.entry("S", strategy.short, when = close > basis and time > start)
 strategy.close("S", when = low < basis)
 
 // === PLOT ===
-plot(basis, color=white, title='ma')
+plot(basis, color=red, title='ma')
+
+p1 = plot(upper, color=blue)
+p2 = plot(lower, color=blue)
+fill(p1, p2)
+
+ntp1 = plot(ntz_up, color=gray)
+ntp2 = plot(ntz_low, color=gray)
+fill(ntp1, ntp2, color=gray)
