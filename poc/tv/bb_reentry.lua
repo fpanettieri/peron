699870a736f059769b1815a6beebb94f3f2c4e32
Fabio Panettieri <fpanettieri@gmail.com>
2019-04-14 11:07:06 -0300

    reentry strategy

---
+++ poc/tv/bb_reentry.lua
@@ -0,0 +1,25 @@
+//@version=3
+strategy("BB RE ENTRY", overlay=true)
+
+// === INPUT ===
+period = input(20, minval=1)
+mult = input(2, minval=0.001, maxval=50)
+
+// === PARAMS ===
+basis = sma(close, period)
+dev = mult * stdev(close, period)
+upper = basis + dev
+lower = basis - dev
+
+// === EXECUTION ===
+strategy.entry("L", strategy.long, when = crossover(close, lower) and close < basis)
+strategy.close("L", when = high > basis)
+
+strategy.entry("S", strategy.short, when = crossunder(close, upper) and close > basis)
+strategy.close("S", when = low < basis)
+
+// === PLOT ===
+plot(basis, color=white, title='ma')
+p1 = plot(upper, color=white, title='upper')
+p2 = plot(lower, color=white, title='lower')
+fill(p1, p2)
