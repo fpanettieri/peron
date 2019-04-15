8d68d851356ca3df20105bf89f8ae1adb104dc4a
Fabio Panettieri <fpanettieri@gmail.com>
2019-04-15 16:56:52 -0300

    strategies reorg

--- poc/tv/bb_re_ntz.lua
+++ poc/tv/bb_re_ntz.lua
@@ -8,14 +8,8 @@ period      = input(20, minval=1)
 src         = input(close, title="Source")
 
 // === INPUT BB ===
-bb_mul  = input(2.0, minval = 0.001, maxval = 50)
+bb_mul  = input(1.7, minval = 0.001, maxval = 50)
 ntz_mul = input(0.7, minval = 0.001, maxval = 50)
-
-// === PARAMS TIME ===
-basis     = sma(src, period)
-start     = timestamp(from_year, from_month, from_day, 00, 00)
-finish    = timestamp(to_year, to_month, to_day, 23, 59)
-window()  => time >= start and time <= finish ? true : false
 
 // === PARAMS BB ===
 bb_dev = bb_mul * stdev(src, period)
@@ -28,11 +22,11 @@ ntz_up = basis + nt_dev
 ntz_low = basis - nt_dev
 
 // === EXECUTION ===
-strategy.entry("L", strategy.long, when = window() and crossover(src, lower) and src < ntz_low)
+strategy.entry("L", strategy.long, when = crossover(src, lower) and src < ntz_low)
 strategy.close("L", when = crossover(src, upper))
 
-strategy.entry("S", strategy.short, when = window() and crossunder(src, upper) and src > ntz_up)
-strategy.close("S", when = crossover(src, lower))
+strategy.entry("S", strategy.short, when = crossunder(src, upper) and src > ntz_up)
+strategy.close("S", when = crossunder(src, lower))
 
 // === PLOT ===
 plot(basis, color=red, title='ma')
