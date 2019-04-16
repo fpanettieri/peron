34aab46d62bb3f3be4f71f5903083a846b634a45
Fabio Panettieri <fpanettieri@gmail.com>
2019-04-16 18:17:16 -0300

    bb implementation

--- poc/core/executor/bb.js
+++ poc/core/executor/bb.js
@@ -13,34 +13,6 @@ let candle = null;
 async function plug (_bb)
 {
   bb = _bb;
-
-  bb.on('QuoteSynced', onQuoteUpdated);
-  bb.on('QuoteOpened', onQuoteUpdated);
-  bb.on('QuoteUpdated', onQuoteUpdated);
-
-  bb.on('CandleAnalyzed', onCandleAnalyzed);
-  bb.on('PositionSynced', onPositionUpdated);
-  bb.on('PositionUpdated', onPositionUpdated);
 }
-
-function onQuoteUpdated (arr)
-{
-  quote = arr[arr.length - 1];
-  log.log('Quote Updated');
-  // TODO: if there is an open order, and the price doesn't match, amend it
-}
-
-function onCandleAnalyzed (c)
-{
-  candle = c;
-  log.log('Candle Analyzed');
-  // TODO: do I need to do anything here?
-}
-
-function onPositionUpdated (p)
-{
-  log.log('Position Updated');
-}
-
 
 module.exports = { plug: plug };
