49786bd484c3452ec1f87d401e27e45a0dbde062
Fabio Panettieri <fpanettieri@gmail.com>
2019-04-15 21:06:56 -0300

    some callbacks, wip

--- poc/core/executor/bb.js
+++ poc/core/executor/bb.js
@@ -6,10 +6,33 @@ const logger = require('../lib/logger');
 const log = new logger('executor/bb');
 
 let bb = null;
+let quote = {};
+let candle = null;
 
 async function plug (_bb)
 {
   bb = _bb;
+
+  bb.on('QuoteSynced', onQuoteUpdated);
+  bb.on('QuoteOpened', onQuoteUpdated);
+  bb.on('QuoteUpdated', onQuoteUpdated);
+
+  bb.on('CandleAnalyzed', onCandleAnalyzed);
+  bb.on('PositionSynced', onPositionUpdated);
+  bb.on('PositionUpdated', onPositionUpdated);
 }
+
+function onQuoteUpdated (arr)
+{
+  quote = arr[arr.length - 1];
+  // TODO: if there is an open order, and the price doesn't match, amend it
+}
+
+function onCandleAnalyzed (c)
+{
+  candle = c;
+  // TODO: do I need to do anything here?
+}
+
 
 module.exports = { plug: plug };
