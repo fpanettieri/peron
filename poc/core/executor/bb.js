d55e62f6b8365dbd344afe641204faa972042fa3
Fabio Panettieri <fpanettieri@gmail.com>
2019-04-16 19:23:55 -0300

    events order

--- poc/core/executor/bb.js
+++ poc/core/executor/bb.js
@@ -13,6 +13,21 @@ let candle = null;
 async function plug (_bb)
 {
   bb = _bb;
+
+  bb.on('TradeContract', onTradeContract);
 }
+
+async function onTradeContract (sym, qty, px)
+{
+  // case 0: no order
+    // sync create an order in the desired direction
+    // check if the order
+  //
+}
+
+// Interesting events:
+//  Trade Contract event
+//  Quote changed
+//  Candle closed
 
 module.exports = { plug: plug };
