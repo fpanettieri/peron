dee977881a63d33e5348ec3402bfbd247d4d2030
Fabio Panettieri <fpanettieri@gmail.com>
2019-04-19 17:52:10 -0300

    logging jobs

--- poc/core/executor/bb.js
+++ poc/core/executor/bb.js
@@ -111,6 +111,9 @@ function createJob (id, sym, qty, px, state, t)
 {
   const job = { id: id, sym: sym, qty: qty, px: px, state: state, t: t, created_at: Date.now()};
   jobs.push(job);
+
+  log.log('job created', JSON.stringify(job));
+
   return job;
 }
 
@@ -118,11 +121,15 @@ function updateJob (id, changes)
 {
   const idx = jobs.findIndex(j => j.id == id);
   jobs[idx] = {...jobs[idx], ...changes};
+
+  log.log('job updated', JSON.stringify(job));
+
   return jobs[idx];
 }
 
 function destroyJob (job)
 {
+  log.log('job destroyed', JSON.stringify(job));
   return jobs.splice(jobs.findIndex(j => j.id === job.id), 1);
 }
 
