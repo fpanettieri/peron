2a35ae8afad69e52c66698d71d7ffc8d01685f0b
Fabio Panettieri <fpanettieri@gmail.com>
2019-05-12 12:21:42 -0300

    better market maker strategy

---
+++ poc/core/brain/bb_mm.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const cfg = require('../../cfg/peron');
+
+const logger = require('../../lib/logger');
+const log = new logger('brain/bb_mm');
+
+let bb = null;
+let previous = null;
+let current = null;
+
+let skip = cfg.brain.skip;
+
+function plug (_bb)
+{
+  bb = _bb;
+  bb.on('CandleAnalyzed', onCandleAnalyzed);
+}
+
+function onCandleAnalyzed (c)
+{
+  previous = current;
+  current = c;
+  if (!previous) { return; }
+  if (skip--) { return; }
+
+  if (c.c > c.bb_upper) {
+    bb.emit('OpenShort', c);
+  }
+
+  if (c.c < c.bb_lower) {
+    bb.emit('OpenLong', c);
+  }
+
+  skip = cfg.brain.skip;
+}
+
+module.exports = { plug: plug };
