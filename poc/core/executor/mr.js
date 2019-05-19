7450e3050ea7e4aa4173f3550666f75a34c69eff
Fabio Panettieri <fpanettieri@gmail.com>
2019-05-18 23:38:53 -0300

    rebranded mean reversar executor

--- poc/core/executor/mr.js
+++ poc/core/executor/mr.js
@@ -4,7 +4,7 @@ const cfg = require('../../cfg/peron');
 const orders = require('../../lib/orders');
 const logger = require('../../lib/logger');
 
-const log = new logger('executor/amm');
+const log = new logger('executor/mr');
 
 const OVERLOAD_STEP = 1000;
 const SLIPPAGE_OFFSET = 100;
