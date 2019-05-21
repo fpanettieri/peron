30c732c7251686872a56f674b8b1c2955ff34509
Fabio Panettieri <fpanettieri@gmail.com>
2019-05-20 23:09:15 -0300

    long slower trades

--- poc/tv/strategy/vix_mr.lua
+++ poc/tv/strategy/vix_mr.lua
@@ -1,41 +1,37 @@
 //@version=3
 
-strategy("BB MR", overlay=true, pyramiding=50, default_qty_type=strategy.percent_of_equity, default_qty_value=2)
+strategy("VIX MR", overlay=true, pyramiding=10, default_qty_type=strategy.percent_of_equity, default_qty_value=10)
 
-// === INPUT GENERAL ===
-period    = input(20, minval=1)
-src       = input(close, title="Source")
+// === INPUT ===
+ma_prd     = input(20, minval=1)
+vix_prd    = input(20, minval=1)
+min_vol    = input(1.0, title='Min Volatility', type=float)
+max_vol    = input(2.0, title='Max Volatility', type=float)
+sl         = input(1.0, title='Stop Loss %', type=float) / 100
 
 // === INPUT TIME ===
-year      = input(defval = 2019, title = "Year", minval = 2017)
-month     = input(defval = 5, title = "Month", minval = 1, maxval = 12)
-day       = input(defval = 20, title = "Day", minval = 1, maxval = 31)
+year       = input(defval = 2019, title = "Year", minval = 2017)
+month      = input(defval = 5, title = "Month", minval = 1, maxval = 12)
+day        = input(defval = 20, title = "Day", minval = 1, maxval = 31)
 
-// === INPUT BB ===
-mul       = input(0.5, minval = 0.001, maxval = 50)
-sl        = input(1.0, title='Stop Loss %', type=float) / 100
+// === TIME ===
+start      = timestamp(year, month, day, 00, 00)
+finish     = timestamp(year, month, day, 23, 59)
+window()   => time >= start and time <= finish ? true : false
 
-// === PARAMS TIME ===
-basis     = sma(src, period)
-start     = timestamp(year, month, day, 00, 00)
-finish    = timestamp(year, month, day, 23, 59)
-window()  => time >= start and time <= finish ? true : false
-
-// === PARAMS BB ===
-dev = mul * stdev(src, period)
-upper = basis + dev
-lower = basis - dev
+// === PARAMS ===
+basis      = ema(close, ma_prd)
+vix_top    = (high - lowest(close, vix_prd)) / (lowest(close, vix_prd)) * 100
+vix_bot    = (highest(close, vix_prd) - low) / (highest(close, vix_prd)) * 100
+open_long  = vix_bot >= min_vol and vix_bot < max_vol
+open_short = vix_top >= min_vol and vix_top < max_vol
 
 // === EXECUTION ===
-strategy.entry("L", strategy.long, when = window() and close < lower, stop = strategy.position_avg_price * (1 - sl))
-strategy.exit("L", when = crossover(close, basis))
+strategy.entry("L", strategy.long, when = window() and close < basis and open_long, stop = strategy.position_avg_price * (1 - sl))
+strategy.close("L", when = crossover(close, basis))
 
-strategy.entry("S", strategy.short, when = window() and close > upper, stop = strategy.position_avg_price * (1 + sl))
-strategy.exit("S", when = crossunder(close, basis))
+strategy.entry("S", strategy.short, when = window() and close > basis and open_short, stop = strategy.position_avg_price * (1 + sl))
+strategy.close("S", when = crossunder(close, basis))
 
 // === PLOT ===
-plot(basis, color=red, title='ma')
-
-p1 = plot(upper, color=blue)
-p2 = plot(lower, color=blue)
-fill(p1, p2)
+plot(basis, color=#A84CB9, linewidth=3, transp=0, title='ma')
