0e0e45379d8ae1a6e2502b7ffc95ac29781443dd
Fabio Panettieri <fpanettieri@gmail.com>
2019-05-20 22:10:21 -0300

    vix ma strat

--- poc/tv/indicator/vix.lua
+++ poc/tv/indicator/vix.lua
@@ -4,18 +4,11 @@ study("VIX", overlay=false)
 
 // === INPUT GENERAL ===
 vix_prd = input(20, minval=1)
-ma_prd = input(100, minval=1)
 
 // === VIX ===
 vix_top = (high - lowest(close, vix_prd)) / (lowest(close, vix_prd)) * 100
 vix_bot = (highest(close, vix_prd) - low) / (highest(close, vix_prd)) * 100
 
-// === MA ===
-ma_top = sma(vix_top, 100)
-ma_top = sma(src, 100)
-
 // === PLOT ===
-plot(vix_top, color=white)
-plot(vix_bot, color=red)
-plot(ma_top, color=blue)
-plot(ma_bot, color=green)
+plot(vix_top, color=#F1453D)
+plot(vix_bot, color=#50AD55)
