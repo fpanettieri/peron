807e258cb569d1f323472fa8c169de450ff33249
Fabio Panettieri <fpanettieri@gmail.com>
2019-05-22 16:17:35 -0300

    better loading error reporting

--- poc/peron.js
+++ poc/peron.js
@@ -13,20 +13,26 @@ const modules = [];
 
   const bb = new backbone();
 
-  for (let i = 0; i < cfg.modules.length; i++) {
-    const name = cfg.modules[i];
-    const m = require(`./${name}`);
-    log.log(`loading module ${name}`);
+  try {
+    for (let i = 0; i < cfg.modules.length; i++) {
+      const name = cfg.modules[i];
+      const m = require(`./${name}`);
+      log.log(`loading module ${name}`);
 
-    m.plug(bb);
-    modules.push(m);
+      m.plug(bb);
+      modules.push(m);
+    }
+
+    for (let i = 0; i < cfg.backbone.chain.length; i++) {
+      const io = cfg.backbone.chain[i];
+      log.log(`chaining ${io[0]} => ${io[1]}`);
+      bb.chain(io[0], io[1]);
+    }
+
+    bb.emit(cfg.backbone.emit);
+
+  } catch (err) {
+    log.fatal(err);
   }
-
-  for (let i = 0; i < cfg.backbone.chain.length; i++) {
-    const io = cfg.backbone.chain[i];
-    log.log(`chaining ${io[0]} => ${io[1]}`);
-    bb.chain(io[0], io[1]);
-  }
-
-  bb.emit(cfg.backbone.emit);
+  
 })();
