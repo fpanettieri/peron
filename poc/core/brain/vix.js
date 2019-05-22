89363a38f13d1c4f6576939e06b6edf21211fa03
Fabio Panettieri <fpanettieri@gmail.com>
2019-05-22 19:43:34 -0300

    vix fix brain

---
+++ poc/core/brain/vix.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const cfg = require('../../cfg/peron');
+
+const logger = require('../../lib/logger');
+const log = new logger('brain/vix');
+
+let bb = null;
+
+function plug (_bb)
+{
+  bb = _bb;
+  bb.on('CandleAnalyzed', onCandleAnalyzed);
+}
+
+function onCandleAnalyzed (c)
+{
+  if (Math.random() >= 0.5) {
+    bb.emit('OpenShort', c);
+  } else {
+    bb.emit('OpenLong', c);
+  }
+}
+
+module.exports = { plug: plug };
