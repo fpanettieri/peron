2e3c0400e545f164ee23a691869d8aeb8f5d4e51
Fabio Panettieri <fpanettieri@gmail.com>
2019-05-23 08:40:04 -0300

    labels

--- poc/tv/indicator/min_max.lua
+++ poc/tv/indicator/min_max.lua
@@ -5,5 +5,5 @@ study("Min/Max", overlay=true)
 period = input(20, minval=1)
 
 // === PLOT ===
-plot(highest(close, period), color=#F1453D, transp=30)
-plot(lowest(close, period), color=#50AD55, transp=30)
+plot(highest(close, period), color=#F1453D, transp=30, linewidth=4, title='Max')
+plot(lowest(close, period), color=#50AD55, transp=30, linewidth=4, title='Min')
