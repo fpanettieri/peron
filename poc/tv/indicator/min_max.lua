de7e10d7e02fa94667353c755961edc0047f8952
Fabio Panettieri <fpanettieri@gmail.com>
2019-05-22 23:48:41 -0300

    rollback

--- poc/tv/indicator/min_max.lua
+++ poc/tv/indicator/min_max.lua
@@ -4,11 +4,6 @@ study("Min/Max", overlay=true)
 // === INPUT GENERAL ===
 period = input(20, minval=1)
 
-// === MIN / MAX ===
-min = lowest(close, period)
-max = highest(close, period)
-
 // === PLOT ===
-plot(min, color=#50AD55, transp=30)
-plot(max, color=#F1453D, transp=30)
-fill(min, max)
+plot(highest(close, period), color=#F1453D, transp=30)
+plot(lowest(close, period), color=#50AD55, transp=30)
