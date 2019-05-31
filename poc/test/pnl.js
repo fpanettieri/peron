312d1c32f17716ac8d0024bd70af8b5859e8fb51
Fabio Panettieri <fpanettieri@gmail.com>
2019-05-31 06:47:14 -0300

    pnl wip

--- poc/test/pnl.js
+++ poc/test/pnl.js
@@ -5,10 +5,15 @@ const assert = require('assert');
 const logger = require('../lib/logger');
 const log = new logger('test/pnl');
 
+const STB = 0.00000001;
+
 async function load_local_execs ()
 {
-  const execs = require('../doc/execution.js');
-  log.log(execs.length);
+  const execs = require('../doc/executions.js');
+  log.log('execs count:', execs.length);
+
+  const acc = execs.reduce((acc, e) => acc + e.homeNotional, 0);
+  log.log(acc);
 }
 
 (async () => {
