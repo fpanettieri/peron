67628768c29ff5f61223d6799694387845d31bac
Fabio Panettieri <fpanettieri@gmail.com>
2019-12-20 04:13:31 -0300

    manual trader config

--- poc/core/backbone.js
+++ poc/core/backbone.js
@@ -16,19 +16,12 @@ class Backbone extends EventEmitter
     this.on(a, () => this.emit(b));
   }
 
-  // Serialize event resolution
   emit ()
   {
     this.queue.push(arguments);
     if (this.queue.length > 1) { return; }
 
     while (this.queue.length > 0) {
-      // FIXME: check if this needs to be removed
-      if (cfg.backbone.verbose) {
-        const msg = this.queue[0][0];
-        if (!cfg.backbone.ignore.includes(msg)) { log.log(msg); }
-      }
-
       super.emit.apply(this, this.queue[0]);
       this.queue.shift();
     }
